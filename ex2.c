efb1d4b4bb6185184cedee22e0ebdbcb0e6fc4e6
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-10-26 17:53:17 +0300

    Add birthday greeting based on user age input

--- ex2.c
+++ ex2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int age ;
+ printf("How old are you? ");
+   scanf("%d" , &age);
+for( int i = 0; i < age; i++){
+printf("Happy birthday to you!\n");
+}
+return 0;
+}
 
