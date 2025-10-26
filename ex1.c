c1d07888c7ef854e34b8d1d31cd8b1e02b9d33d7
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-10-26 16:48:27 +0300

    Add number guessing functionality in ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,12 @@
-
+#include <cs.h>
+#include <stdio.h>
+ int main(void)
+{
+  int number = get_int("Please choose a number between 1-5.");
+  if (number == 3){
+printf("You got it right.\n");
+}
+else {
+ printf("Sorry,try again.\n");
+}
+}
