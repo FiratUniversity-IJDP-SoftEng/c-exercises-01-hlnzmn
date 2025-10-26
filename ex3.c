c7c009816e39c4d60e942ce7f3fed9ade643b7a5
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-10-26 18:11:18 +0300

    Add pet response program for cat and dog

--- ex3.c
+++ ex3.c
@@ -1,2 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main (void){
+  string pet = get_string("Do you have cat or dog? ");
+  if(strcmp( pet, "cat") == 0)
+  {
+    printf("Meow!\n");
+  }
+  else if(strcmp(pet, "dog")== 0){
+    printf("Woof!\n");
+      }
+else{
+  printf("I do not know that pet!\n");
+}
+return 0;
+}
 
 
