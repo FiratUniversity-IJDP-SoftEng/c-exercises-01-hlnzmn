441a8e9e61c257a64bfb7043b428c6dd5675fbde
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-10-27 18:30:21 +0300

    Add hello world function with user input

--- ex5.c
+++ ex5.c
@@ -1,1 +1,11 @@
-
+#include <cs50.h>
+#include <stdio.h>
+void sayHello(string name)
+{
+  printf ("Hello %s\n",name);
+}
+int main(void)
+{
+  string name=get_string("What is your name?");
+ sayHello(name);
+}
